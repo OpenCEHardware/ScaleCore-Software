00fe4404cb1ae5ca80ffd81f33d47cb3fc972fae
Alejandro Soto <alejandro@34project.org>
2024-10-15 23:25:42 -0600

    Add bin2hex hook call in mod.mk

--- sw/mod.mk
+++ sw/mod.mk
@@ -1,20 +1,23 @@
-cores := hsv_host_image hsv_host_sw hsv_picolibc
+cores := hsv_host_sw hsv_picolibc
 subdirs := machine user
 
 define core/hsv_host_sw
   $(this)/deps := hsv_machine_sw hsv_user_sw hsv_picolibc
   $(this)/cross := riscv32-none-elf-
-  $(this)/hooks := cc objcopy obj
+  $(this)/hooks := cc objcopy makehex obj
 
   $(this)/obj_deps := picolibc/picolibc.specs
 
   $(this)/cc_flags  = $$(if $$(enable_opt),-O3 -ffast-math) -g \
                       -march=rv32i_zicsr -mabi=ilp32 --specs=$$(obj)/picolibc/picolibc.specs
   $(this)/ld_flags := --oslib=semihost
-  $(this)/ld_binary := hsv_host_image
+  $(this)/ld_binary := hsv_host_sw
 
-  $(this)/objcopy_src := hsv_host_image
-  $(this)/objcopy_obj := hsv_host_image.bin
+  $(this)/objcopy_src := hsv_host_sw
+  $(this)/objcopy_obj := hsv_host_sw.bin
+
+  $(this)/makehex_src := hsv_host_sw.bin
+  $(this)/makehex_obj := hsv_host_sw.hex
 endef
 
 define core/hsv_picolibc
